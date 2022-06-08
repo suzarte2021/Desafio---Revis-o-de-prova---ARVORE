7b1e20ac79df6dcda7f9bf6d23e3883f327317f6
Misael Suzarte <84162098+suzarte2021@users.noreply.github.com>
2022-06-08 18:19:32 -0300

    Update Arvore.java

--- Arvore.java
+++ Arvore.java
@@ -4,31 +4,31 @@ import java.util.Stack;
 
 public class Arvore {
 	
-/*/Escreva um programa que leia 10 números, armazene-os em uma árvore e,
-*  em seguida, liste apenas os números pares.
+/*/Escreva um programa que leia 10 nÃºmeros, armazene-os em uma Ã¡rvore e,
+*  em seguida, liste apenas os nÃºmeros pares.
 *  Pode utilizar qualquer caminhamento.	
  /*/
 	public static void main(String[] args) {	
-		Stack<Integer> pilhaPar = new Stack<Integer>(); //Stack é uma pilha para armazenar
+		Stack<Integer> pilhaPar = new Stack<Integer>(); //Stack Ã© uma pilha para armazenar
 		Scanner scanner = new Scanner(System.in); // Entrada de dados
 		boolean exit = true;
-		while(exit == true) { // Estrutura de condição
+		while(exit == true) { // Estrutura de condiÃ§Ã£o
 
-			System.out.println("Digite um número 0 para sair"); // 
+			System.out.println("Digite um nÃºmero 0 para sair"); // 
 			int num = scanner.nextInt();        //Recebe o dado colocado
 			if(num == 0) { // Validando o Lop, para sair
 				exit = false;
 			} else {
 
-				if(num % 2 == 0) { // Se o núemero for par
-					System.out.println("Esse número é Par");
-					pilhaPar.push(num); // Inserção/Inserindo elemento
+				if(num % 2 == 0) { // Se o nÃºemero for par
+					System.out.println("");
+					pilhaPar.push(num); // InserÃ§Ã£o/Inserindo elemento
 				
 				}
 			}
 		}
 		//Vamos verificar se a pilhaPar esta vazia
-		System.out.println("Verificando se a lista da pilha Par esta vazia: " + pilhaPar.empty()); //Verificar se está vazia
+		System.out.println("Verificando se a lista da pilha Par esta vazia: " + pilhaPar.empty()); //Verificar se estÃ¡ vazia
 		if( pilhaPar.empty()) { //
 			System.out.println("A lista pilha Par esta vazia!!!");
 		} else {
@@ -36,7 +36,7 @@ public class Arvore {
 		}
 
 		//Listando os numeros Pares
-		System.out.println("Os números da lista Par são:"+ pilhaPar.size());
+		System.out.println("Os nÃºmeros da lista Par sÃ£o:"+ pilhaPar.size());
 		 for (int i = 0; i < pilhaPar.size(); i++) {
 				System.out.println(pilhaPar.get(i));
 			}
